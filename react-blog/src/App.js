4699c24bdab45da09a243040d8e19e73aaed5c06
xp080034537z@gmail.com <xp080034537z@gmail.com>
2022-09-21 03:32:37 +0200

    add react-router-dom

--- react-blog/src/App.js
+++ react-blog/src/App.js
@@ -5,12 +5,28 @@ import Register from "./pages/register/Register";
 import Settings from "./pages/settings/Settings";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
+// If you are using react-router-dom v6, it looks like Switch has been replaced with Routes.
 function App() {
+  const user = false;
   return (
-    <>
-    <TopBar/>
-    <Register/>
-    </>
+    <Router>
+        <TopBar/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={user ? <Home/> : <Register/>} />
+          <Route path="/login" element={user? <Home/> : <Login/>} />
+          <Route path="/write" element={user? <Write/> : <Register/>} />
+          <Route path="/settings" element={user? <Settings/> : <Register/>} />
+          <Route path="/post/:postId" element={<Single/>} />
+        </Routes>
+    </Router>
   );
 }
 
--- react-blog/src/components/topbar/TopBar.jsx
+++ react-blog/src/components/topbar/TopBar.jsx
@@ -1,6 +1,8 @@
+import { Link } from "react-router-dom";
 import "./topbar.css"
 
 export default function TopBar() {
+  const user = false;
   return (
     <div className='top'>
         <div className="topLeft">
@@ -11,15 +13,33 @@ export default function TopBar() {
         </div>
         <div className="topCenter">
           <ul className="topList">
-            <li className="topListItem">HOME</li>
-            <li className="topListItem">ABOUT</li>
-            <li className="topListItem">CONTACT</li>
-            <li className="topListItem">WRITE</li>
-            <li className="topListItem">LOGOUT</li>
+            <li className="topListItem">
+              <Link className="link" to="/">HOME</Link>
+            </li>
+            <li className="topListItem"><Link className="link" to="/">ABOUT</Link></li>
+            <li className="topListItem"><Link className="link" to="/">CONTACT</Link></li>
+            <li className="topListItem"><Link className="link" to="/write">WRITE</Link></li>
+            <li className="topListItem">
+              {user && "LOGOUT"}
+            </li>
           </ul>
         </div>
         <div className="topRight">
-          <img className="topImg" src="https://images.pexels.com/photos/13221178/pexels-photo-13221178.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+          {
+            user ? (
+              <img className="topImg" src="https://images.pexels.com/photos/13221178/pexels-photo-13221178.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+            ) : (
+              <ul className="topList">
+                <li className="topListItem">
+                  <Link className="link" to="/login">LOGIN</Link>
+                </li>
+                <li className="topListItem">
+                  <Link className="link" to="/register">REGISTER</Link>
+                </li>
+              </ul>
+            )
+          }
+          
           <i className="topSearchIcon fa-solid fa-magnifying-glass"></i>
         </div>
     </div>
