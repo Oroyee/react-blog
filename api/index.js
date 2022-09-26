d804f549fa1ed06a96890aae6627cee52240a351
xp080034537z@gmail.com <xp080034537z@gmail.com>
2022-09-26 23:55:11 +0200

    Create MongoDB and api

---
+++ api/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+})
+.then(console.log("Connected to MongoDB"))
+.catch(err=>console.log(err));
+
+app.use("/oro",(req,res)=>{
+    console.log("hey this is oro url")
+})
+
+app.listen("5000",() => {
+    console.log("Backend is running.")
+});
