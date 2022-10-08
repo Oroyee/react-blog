207bc8a4d1af14ff49d5f07ac0f12a70d4d37c4b
xp080034537z@gmail.com <xp080034537z@gmail.com>
2022-10-08 02:01:02 +0200

    fetch data from backend(homepage, single post)

--- react-blog/src/components/singlePost/SinglePost.jsx
+++ react-blog/src/components/singlePost/SinglePost.jsx
@@ -1,33 +1,44 @@
+import axios from "axios";
+import { useState } from "react";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom"
 import "./singlePost.css"
 
 export default function SinglePost() {
+  const location = useLocation();
+  const path = location.pathname.split("/")[2];
+  const [post, setPost] = useState({})
+
+  useEffect(()=>{
+    const getPost = async () => {
+      const res = await axios.get("/posts/"+ path);
+      setPost(res.data)
+    };
+    getPost()
+  },[path]);
+
   return (
     <div className="singlePost">
         <div className="singlePostWrapper">
-          <img className="singlePostImg" src="https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+          {post.photo&&
+            <img 
+              className="singlePostImg" 
+              src={post.photo}
+              alt="" 
+            />
+          }
           <h1 className="singlePostTitle">
-            Lorem ipsum dolor sit, amet consectetur. 
+            {post.title}
             <div className="singlePostEdit">
               <i className="singlePostIcon fa-regular fa-pen-to-square"></i>
               <i className="singlePostIcon fa-regular fa-trash-can"></i>
             </div>
           </h1>
           <div className="singlePostInfo">
-            <span className="singlePostAuthor">Author: <b>Safak</b></span>
-            <span className="singlePostDate">1 hour ago</span>
+            <span className="singlePostAuthor">Author: <b>{post.username}</b></span>
+            <span className="singlePostDate">{new Date(post.createdAt).toDateString()}</span>
           </div>
-          <p className="singlePostDesc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi velit atque aliquam,
-            illo earum expedita provident ipsam natus fugit iusto,
-            quam ipsa laudantium nemo numquam illum perspiciatis pariatur aspernatur qui!
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi velit atque aliquam,
-            illo earum expedita provident ipsam natus fugit iusto,
-            quam ipsa laudantium nemo numquam illum perspiciatis pariatur aspernatur qui!
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi velit atque aliquam,
-            illo earum expedita provident ipsam natus fugit iusto,
-            quam ipsa laudantium nemo numquam illum perspiciatis pariatur aspernatur qui!
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi velit atque aliquam,
-            illo earum expedita provident ipsam natus fugit iusto,
-            quam ipsa laudantium nemo numquam illum perspiciatis pariatur aspernatur qui!</p>
+          <p className="singlePostDesc">{post.desc}</p>
         </div>
     </div>
   )
