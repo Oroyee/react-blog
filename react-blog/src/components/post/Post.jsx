d5e869b22d8cc792c24b3338ef7238da4c331440
xp080034537z@gmail.com <xp080034537z@gmail.com>
2022-10-12 20:56:18 +0200

    update for netlify

--- react-blog/src/components/post/Post.jsx
+++ react-blog/src/components/post/Post.jsx
@@ -1,8 +1,9 @@
 import "./post.css"
-import {Link} from "react-router-dom"
+import {Link, useLocation} from "react-router-dom"
 
 export default function Post({post}) {
   const PF = "https://oroblog.herokuapp.com/images/"
+
   return (
     <div className="post">
       {post.photo && (
