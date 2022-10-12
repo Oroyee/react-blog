67b2d33f102a6b9365a262feea155c1121e09403
xp080034537z@gmail.com <xp080034537z@gmail.com>
2022-10-12 13:32:40 +0200

    update heroku debug log

--- api/index.js
+++ api/index.js
@@ -13,7 +13,7 @@ dotenv.config();
 app.use(express.json());
 app.use("/images", express.static(path.join(__dirname,"images")))
 
-app.get('/', (req, res) => { res.send('Hello from Express!')
+app.get('/', (req, res) => { res.send('Hello from Express!')});
 
 
 mongoose.connect(process.env.MONGO_URL, {
