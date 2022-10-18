70cbd2e9cb08ebe1e9209c02173a967e9dce6ab9
xp080034537z@gmail.com <xp080034537z@gmail.com>
2022-10-18 21:45:39 +0200

    update home page post sort from new to old

--- react-blog/src/components/posts/Posts.jsx
+++ react-blog/src/components/posts/Posts.jsx
@@ -4,7 +4,7 @@ import "./posts.css"
 export default function Posts({ posts} ) {
   return (
     <div className="posts">
-        {posts.map((p) =>(
+        {posts.slice(0).reverse().map((p) =>(
           <Post post={p}/>
         ))}
     </div>
