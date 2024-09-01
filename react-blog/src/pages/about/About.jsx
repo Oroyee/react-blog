20e33ef937a7058e0bc73ded271e901a646c239f
Oroyee <xp080034537z@gmail.com>
2024-09-01 18:55:53 +0200

    about test

--- react-blog/src/pages/about/About.jsx
+++ react-blog/src/pages/about/About.jsx
@@ -4,25 +4,28 @@ import {useSpring, animated} from 'react-spring';
 import './about.css'
 import Slider from '../../components/slider/Slider'
 
+import { useRef } from 'react';
+
+import gsap from 'gsap'; // <-- import GSAP
+import { useGSAP } from '@gsap/react'; // <-- import the hook from our React package
+
+gsap.registerPlugin(useGSAP);
 
 export default function About() {
-  const [flip, setFlip] = useState(false);
-  const props = useSpring({
-    to: { opacity: 1 },
-    from: { opacity: 0 },
-
-  });
+  const container = useRef();
+  useGSAP(
+    () => {
+        // gsap code here...
+        gsap.to('.box', { rotation: 180 }); // <-- automatically reverted
+    },
+    { scope: container }
+); // <-- scope for selector text (optional)
 
   return (
     <div className='main'>
-      
-      <div className="overlay"></div>
-      <video className="video" src={videoBG} autoPlay playsInline loop muted></video>
-      <div className='content'>
-        <div className="desc">
-          <Slider/>
+       <div ref={container} className="app">
+            <div className="box">Hello</div>
         </div>
-      </div>
     </div>
   )
 }
